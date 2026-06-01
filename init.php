ed9f226571e8a412eff1e5e7f8a7fdc729de0eaf
P6g9YHK6 <P6g9YHK6@users.noreply.github.com>
2026-06-01 16:06:09 +0200

    Inline plugin JS in hook_prefs_tab instead of get_js()

--- init.php
+++ init.php
@@ -24,10 +24,6 @@ class Fu_Cloudflare extends Plugin {
 		$host->add_hook($host::HOOK_PREFS_SAVE_FEED, $this);
 	}
 
-	function get_js() {
-		return file_get_contents(__DIR__ . "/init.js");
-	}
-
 	function hook_prefs_tab($args) {
 		if ($args != "prefFeeds") return;
 
@@ -180,6 +176,72 @@ class Fu_Cloudflare extends Plugin {
 				</ul>
 			<?php } ?>
 		</div>
+		<script type="text/javascript">
+			window.Plugins = window.Plugins || {};
+			window.Plugins.Fu_Cloudflare = {
+				testConnection: function() {
+					var url = dijit.byId("fu_test_url").get("value");
+					if (!url) { alert("Enter a URL to test."); return; }
+					Notify.progress('Testing FlareSolverr connection...', true);
+					xhr.post("backend.php", {
+						op: "PluginHandler",
+						plugin: "fu_cloudflare",
+						method: "testConnection",
+						test_url: url
+					}, function(reply) {
+						var div = document.getElementById("fu_test_result");
+						try {
+							var result = JSON.parse(reply);
+							if (result.success) {
+								div.innerHTML = "<div class='notice alert alert-info'><strong>Success!</strong> " +
+									result.time + "s, " + result.size + " bytes" +
+									(result.title ? "<br>Feed: " + result.title : "") + "</div>";
+							} else {
+								div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
+							}
+						} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
+					});
+				},
+				testFlareSolverr: function() {
+					Notify.progress('Testing FlareSolverr...', true);
+					xhr.post("backend.php", {
+						op: "PluginHandler",
+						plugin: "fu_cloudflare",
+						method: "testFlareSolverr"
+					}, function(reply) {
+						var div = document.getElementById("fu_flaresolverr_result");
+						try {
+							var result = JSON.parse(reply);
+							if (result.success) {
+								div.innerHTML = "<div class='notice alert alert-info'><strong>FlareSolverr is reachable!</strong> " +
+									"v" + result.version + ", " + result.time + "s response time</div>";
+							} else {
+								div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
+							}
+						} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
+					});
+				},
+				purgeSmartList: function() {
+					Notify.progress('Purging smart-added feeds...', true);
+					xhr.post("backend.php", {
+						op: "PluginHandler",
+						plugin: "fu_cloudflare",
+						method: "clearSmartList"
+					}, function(reply) {
+						var div = document.getElementById("fu_purge_result");
+						try {
+							var result = JSON.parse(reply);
+							if (result.success) {
+								div.innerHTML = "<div class='notice alert alert-info'>" + result.message + "</div>";
+								Plugins.reload();
+							} else {
+								div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
+							}
+						} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
+					});
+				}
+			};
+		</script>
 		<?php
 	}
 
