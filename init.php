d07dff1b3fa8f7888c9f75975a4759f57bc76009
P6g9YHK6 <P6g9YHK6@users.noreply.github.com>
2026-06-01 16:13:19 +0200

    Merge Plugins.Fu_Cloudflare into existing dojo/method block inside AccordionPane
    The standalone dojo/method block at the end of hook_prefs_tab output
    was outside the AccordionPane widget (after the closing </div>).
    Dojo only processes dojo/method scripts that are direct children of
    widget nodes during parsing. Moved the definition into the existing
    working dojo/method block that sits inside the pane.

--- init.php
+++ init.php
@@ -119,6 +119,64 @@ class Fu_Cloudflare extends Plugin {
 						});
 					}
 				});
+
+				window.Plugins = window.Plugins || {};
+				window.Plugins.Fu_Cloudflare = {
+					testConnection: function() {
+						var url = dijit.byId("fu_test_url").get("value");
+						if (!url) { alert("Enter a URL to test."); return; }
+						Notify.progress('Testing FlareSolverr connection...', true);
+						xhr.post("backend.php", {
+							op: "PluginHandler", plugin: "fu_cloudflare", method: "testConnection", test_url: url
+						}, function(reply) {
+							var div = document.getElementById("fu_test_result");
+							try {
+								var result = JSON.parse(reply);
+								if (result.success) {
+									div.innerHTML = "<div class='notice alert alert-info'><strong>Success!</strong> " +
+										result.time + "s, " + result.size + " bytes" +
+										(result.title ? "<br>Feed: " + result.title : "") + "</div>";
+								} else {
+									div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
+								}
+							} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
+						});
+					},
+					testFlareSolverr: function() {
+						Notify.progress('Testing FlareSolverr...', true);
+						xhr.post("backend.php", {
+							op: "PluginHandler", plugin: "fu_cloudflare", method: "testFlareSolverr"
+						}, function(reply) {
+							var div = document.getElementById("fu_flaresolverr_result");
+							try {
+								var result = JSON.parse(reply);
+								if (result.success) {
+									div.innerHTML = "<div class='notice alert alert-info'><strong>FlareSolverr is reachable!</strong> " +
+										"v" + result.version + ", " + result.time + "s response time</div>";
+								} else {
+									div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
+								}
+							} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
+						});
+					},
+					purgeSmartList: function() {
+						Notify.progress('Purging smart-added feeds...', true);
+						xhr.post("backend.php", {
+							op: "PluginHandler", plugin: "fu_cloudflare", method: "clearSmartList"
+						}, function(reply) {
+							var div = document.getElementById("fu_purge_result");
+							try {
+								var result = JSON.parse(reply);
+								if (result.success) {
+									div.innerHTML = "<div class='notice alert alert-info'>" + result.message + "</div>";
+									Plugins.reload();
+								} else {
+									div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
+								}
+							} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
+						});
+					}
+				};
 			</script>
 
 			<hr/>
@@ -176,72 +234,6 @@ class Fu_Cloudflare extends Plugin {
 				</ul>
 			<?php } ?>
 		</div>
-		<script type="dojo/method">
-			window.Plugins = window.Plugins || {};
-			window.Plugins.Fu_Cloudflare = {
-				testConnection: function() {
-					var url = dijit.byId("fu_test_url").get("value");
-					if (!url) { alert("Enter a URL to test."); return; }
-					Notify.progress('Testing FlareSolverr connection...', true);
-					xhr.post("backend.php", {
-						op: "PluginHandler",
-						plugin: "fu_cloudflare",
-						method: "testConnection",
-						test_url: url
-					}, function(reply) {
-						var div = document.getElementById("fu_test_result");
-						try {
-							var result = JSON.parse(reply);
-							if (result.success) {
-								div.innerHTML = "<div class='notice alert alert-info'><strong>Success!</strong> " +
-									result.time + "s, " + result.size + " bytes" +
-									(result.title ? "<br>Feed: " + result.title : "") + "</div>";
-							} else {
-								div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
-							}
-						} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
-					});
-				},
-				testFlareSolverr: function() {
-					Notify.progress('Testing FlareSolverr...', true);
-					xhr.post("backend.php", {
-						op: "PluginHandler",
-						plugin: "fu_cloudflare",
-						method: "testFlareSolverr"
-					}, function(reply) {
-						var div = document.getElementById("fu_flaresolverr_result");
-						try {
-							var result = JSON.parse(reply);
-							if (result.success) {
-								div.innerHTML = "<div class='notice alert alert-info'><strong>FlareSolverr is reachable!</strong> " +
-									"v" + result.version + ", " + result.time + "s response time</div>";
-							} else {
-								div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
-							}
-						} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
-					});
-				},
-				purgeSmartList: function() {
-					Notify.progress('Purging smart-added feeds...', true);
-					xhr.post("backend.php", {
-						op: "PluginHandler",
-						plugin: "fu_cloudflare",
-						method: "clearSmartList"
-					}, function(reply) {
-						var div = document.getElementById("fu_purge_result");
-						try {
-							var result = JSON.parse(reply);
-							if (result.success) {
-								div.innerHTML = "<div class='notice alert alert-info'>" + result.message + "</div>";
-								Plugins.reload();
-							} else {
-								div.innerHTML = "<div class='notice alert alert-warning'><strong>Error:</strong> " + result.error + "</div>";
-							}
-						} catch(e) { div.innerHTML = "<div class='notice alert alert-warning'>" + reply + "</div>"; }
-					});
-				}
-			};
-		</script>
 		<?php
 	}
 
